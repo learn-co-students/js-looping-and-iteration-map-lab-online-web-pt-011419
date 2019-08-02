eb28a48d9684d953beeb04ed33cb2ae4c742def9
dclements9 <dclements@protonmail.com>
2019-08-02 13:06:19 -0400

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 function lowerCaseDrivers(drivers){
-    debugger;
     return drivers.map(function(driver){
-        debugger;
         return driver.toLowerCase();
     })
 }
-const array = ["Bob", "Alice", "Dylan"]
-lowerCaseDrivers(array);
+
+function nameToAttributes(drivers){
+    return drivers.map(function(driver){
+        return {firstName: driver.split(' ')[0], lastName: driver.split(' ')[1]}
+    })
+}
+
+function attributesToPhrase(drivers){
+    return drivers.map(function(driver) {
+        return `${driver.name} is from ${driver.hometown}`;
+    })
+}
