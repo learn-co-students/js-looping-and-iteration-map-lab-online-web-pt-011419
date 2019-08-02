d23b56aef6747c6dbdfc85b69dfc1963b5808aef
Dylan Clements <dclements@protonmail.com>
2019-08-02 11:22:35 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Code your solution in this file.
+function lowerCaseDrivers(drivers){
+    debugger;
+    return drivers.map(function(driver){
+        debugger;
+        return driver.toLowerCase();
+    })
+}
+const array = ["Bob", "Alice", "Dylan"]
+lowerCaseDrivers(array);
