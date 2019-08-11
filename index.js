87cd7839fa8a831ddce516049a323cea1b2a59c1
NYHolzer <nissan.holzer@gmail.com>
2019-08-11 18:16:33 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file.
+function lowerCaseDrivers(array) {
+    
+    const lowerCase = array.map(function(element) {
+        return element.toLowerCase()
+    })
+
+    return lowerCase
+}
+
+function nameToAttributes(array) {
+
+    const arrayAttributes = array.map ( function (element) {
+        let splitElement = element.split(' ')
+        return {firstName: splitElement[0], lastName: splitElement[1]}
+    });
+
+    return arrayAttributes;
+}
+
+function attributesToPhrase(array) {
+    let phrase = array.map(function(obj){
+        return `${obj.name} is from ${obj.hometown}`
+    })
+
+    return phrase
+}
